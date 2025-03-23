044541320834bf1939fbdd91b047b35854fd0ffb
bnusmann22 <bn.usmann22@gmail.com>
2025-03-23 13:12:42 +0100

    more changes

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,52 @@
-document.querySelectorAll('.drum').forEach((btn) => {
-  btn.addEventListener('click', handleClick);
-});
+let numberOfDrumButtons = document.querySelectorAll('.drum').length;
 
-function handleClick() {
-  alert('Hello !! , I got clicked');
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    let btnInnerHTML = this.innerHTML;
+    console.log(btnInnerHTML);
+    makeSound(btnInnerHTML);
+  });
+}
+
+document.addEventListener('keypress', function (event) {
+  makeSound(event.key);
+});
+function makeSound(key) {
+  switch (key) { 
+    case 'w':
+      let tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play;
+      alert('Tom1 was played');
+      break;
+    case 'a':
+      let tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play;
+      alert('Tom2 was played');
+      break;
+    case 's':
+      let tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play;
+      alert('Tom3 was played');
+      break;
+    case 'd':
+      let tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play;
+      alert('Tom4 was played');
+      break;
+    case 'j':
+      let snare = new Audio('sounds/snare.mp3');
+      snare.play;
+      alert('Snare was played');
+      break;
+    case 'k':
+      let crash = new Audio('sounds/crash.mp3');
+      crash.play;
+      alert('Crash was played');
+      break;
+    case 'l':
+      let kick = new Audio('sounds/kick-bass.mp3');
+      kick.play;
+      alert('KIck was played');
+      break;
+  }
 }
